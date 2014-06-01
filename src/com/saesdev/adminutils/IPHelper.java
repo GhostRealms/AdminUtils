ca21cb4cc0c7ae43e380faa36a4fa3f6b15e1f71
River <rivermarmorstein@gmail.com>
2014-05-31 22:17:13 -0400

    Added IP and UUID Helper Classes

---
+++ src/com/saesdev/adminutils/IPHelper.java
@@ -0,0 +1,27 @@
+package com.saesdev.adminutils;
+
+import java.util.UUID;
+
+public class IPHelper {
+	
+	public boolean hasAlt(UUID uuid) {
+		//In Development - Need Database Connections
+		return false;
+	}
+	
+	public String[] listAlts(UUID uuid) {
+		//In Development - Need Database
+		return null;
+	}
+	
+	public String[] listAccounts(String ipAddress) {
+		//In Development - Need Database
+		return null;
+	}
+	
+	public String getIP(UUID uuid) {
+		//In Development - Need Database
+		return null;
+	}
+
+}
---
+++ src/com/saesdev/adminutils/UUIDHelper.java
@@ -0,0 +1,35 @@
+package com.saesdev.adminutils;
+
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class UUIDHelper {
+	
+	public UUID getPlayerUUID(String username) {
+		Player p = Bukkit.getPlayer(username);
+		
+		UUID UUID = p.getUniqueId();
+		
+		return UUID;
+	}
+	
+	public String getUserfromID(UUID uuid) {
+		Player p = Bukkit.getPlayer(uuid);
+		String username = p.getName();
+		return username;
+	}
+	
+	public boolean hasAlt(UUID uuid) {
+		//In Development - Need Database Connections
+		String sUUID = uuid.toString();
+		return false;
+	}
+	
+	public String[] listAlts(UUID uuid) {
+		//In Development - Need Database Connectons
+		return null;
+	}
+
+}

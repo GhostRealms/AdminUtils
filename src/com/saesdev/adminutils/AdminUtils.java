5a6aa8405034ed5b29f24d8e23dd1764efbb0c9a
River <rivermarmorstein@gmail.com>
2014-05-31 21:59:33 -0400

    Added a Header Note

--- src/com/saesdev/adminutils/AdminUtils.java
+++ src/com/saesdev/adminutils/AdminUtils.java
@@ -1,5 +1,10 @@
 package com.saesdev.adminutils;
 
+/*
+ * AdminUtils is an Administration Utility Plugin for Bukkit. It is licenced under GNU GPLv3.
+ * Authors: Rmarmorstein, Pluto1099, [Add Your Name Here If you Contribute :)]
+ */
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AdminUtils extends JavaPlugin {
